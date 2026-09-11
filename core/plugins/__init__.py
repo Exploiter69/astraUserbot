49e0a7df282fe61b6b1225831177ed259cf85fdd
Alok kumar <98121054+Exploiter69@users.noreply.github.com>
2026-09-11 08:55:39 +0530

    feat: add plugin manager package

---
+++ core/plugins/__init__.py
@@ -0,0 +1,5 @@
+"""Plugin lifecycle and management primitives."""
+
+from core.plugins.manager import PluginManager, PluginRecord, PluginState
+
+__all__ = ["PluginManager", "PluginRecord", "PluginState"]
