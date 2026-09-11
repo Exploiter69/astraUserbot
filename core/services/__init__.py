d66f3a24001da9e32cea51c22b49c8f6ad97e2d0
Alok kumar <98121054+Exploiter69@users.noreply.github.com>
2026-09-11 10:40:29 +0530

    feat: register SecretStore service

--- core/services/__init__.py
+++ core/services/__init__.py
@@ -3,6 +3,7 @@
 from core.services.cache import Artifact, CacheEntry, CacheService, CacheStats
 from core.services.http import HttpService
 from core.services.jobs import Job, JobEngine, JobError, JobState
+from core.services.secrets import SecretStore, SecretStoreError
 from core.services.storage import StorageError, StorageService
 from core.services.subprocess import SubprocessResult, SubprocessService
 from core.services.telegram import TelegramFacade
@@ -12,6 +13,7 @@ __all__ = [
     "Artifact", "CacheEntry", "CacheService", "CacheStats",
     "HttpService",
     "Job", "JobEngine", "JobError", "JobState",
+    "SecretStore", "SecretStoreError",
     "StorageError", "StorageService",
     "SubprocessResult", "SubprocessService",
     "TelegramFacade",
