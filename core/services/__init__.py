59a6e4bf84a2d8f9ba986a3bb83245c25125b611
Alok kumar <98121054+Exploiter69@users.noreply.github.com>
2026-09-14 19:56:12 +0530

    feat: export Telegram event collector

--- core/services/__init__.py
+++ core/services/__init__.py
@@ -13,6 +13,7 @@ from core.services.secrets import SecretStore, SecretStoreError
 from core.services.storage import StorageError, StorageService
 from core.services.subprocess import SubprocessResult, SubprocessService
 from core.services.telegram import TelegramFacade
+from core.services.telegram_events import TelegramEvent, TelegramEventCollector
 from core.services.telegram_state import DialogState, EntityState, TelegramStateCache
 from core.services.telegram_sync import SyncCursor, SyncResult, TelegramIncrementalSync
 from core.services.telegram_traffic import TelegramTrafficController
@@ -32,6 +33,7 @@ __all__ = [
     "SubprocessResult", "SubprocessService",
     "DialogState", "EntityState", "TelegramStateCache",
     "SyncCursor", "SyncResult", "TelegramIncrementalSync",
+    "TelegramEvent", "TelegramEventCollector",
     "TelegramFacade", "TelegramTrafficController",
     "Workspace", "WorkspaceService",
 ]
